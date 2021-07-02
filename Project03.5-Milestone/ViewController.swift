4b66c04ee4731168b5280bd448faffc1ded7a9b7
Paul Willis <ios_dev@paulrobertwillis.com>
2021-07-02 17:11:34 +0100

    Issue linking row pressed to image required

--- Project03.5-Milestone/ViewController.swift
+++ Project03.5-Milestone/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var flags = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.contains("@") {
+                let sanitisedItem = sanitiseString(string: item)
+                flags.append(sanitisedItem)
+            }
+        }
+                
+        flags.sort()
+    }
+    
+    func sanitiseString(string: String) -> String {
+        return string.replacingOccurrences(of: "@2x.png", with: "").uppercased()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = flags[indexPath.row]
+
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
 
